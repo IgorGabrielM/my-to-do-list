ba951278dbfc32b8d51282cbfdd93339ce95727a
igorGabrielM <igorgabprofissional@gmail.com>
2022-09-21 16:19:57 -0300

    fix(index, Home.module): add a style on home page

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "7305561a44daf6fc"; }
+/******/ 		__webpack_require__.h = function() { return "a17fa981c68a4afd"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,31 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Link from "next/link";
 
 const Home: NextPage = () => {
   return (
       <div>
-          <h1 className="text-3xl font-bold underline">
-            Hello world!
-          </h1>
-          <div className='w-14 text-center bg-indigo-500 rounded-md '>
-              <Link href='/auth'>
-                  <a>Login</a>
-              </Link>
+          <div className={styles.header}>
+              <div className={styles.buttons}>
+                  <div className={styles.buttonHeader}>
+                      <Link href='/auth' className={styles.buttonHeader}>
+                          <a>Login</a>
+                      </Link>
+                  </div>
+                  <div className={styles.buttonHeader}>
+                      <Link href='/auth/createUser' className={styles.buttonHeader}>
+                          <a>Criar conta</a>
+                      </Link>
+                  </div>
+              </div>
+          </div>
+          <div className='flex justify-start ml-10 mt-10'>
+              <h1 className="text-3xl font-bold underline">
+                  Bem vindo, ao seu site de produtividade
+              </h1>
+              <div className={styles.backImage}>
+                  <img src="/images/task-amico.png" alt="imagem de tarefas" className={styles.image}/>
+              </div>
           </div>
       </div>
   )
